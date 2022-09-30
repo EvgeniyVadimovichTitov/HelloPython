5059c1fc517f2d5385c9bbd6e2db91c561fe7291
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-30 19:40:44 +0300

    complete task5

--- decision.py
+++ decision.py
@@ -1,1 +1,27 @@
-## задания распределены по веткам
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+list = []
+list_replay = []
+n = random.randint(4, 8)
+
+
+def Mix_List(List):  # перемешивание без оператора shuffle
+    list_mix = []
+    while len(List) > 0:
+        j = random.choice(List)
+        list_mix.append(j)
+        List.remove(j)
+    return list_mix
+
+
+for i in range(1, n):
+    list.append(int(-n/2 + i))
+for i in range(1, n):
+    list_replay.append(int(-n/2 + i))
+
+print(n)
+print(list)
+random.shuffle(list)
+print(list)
+print(Mix_List(list_replay))
