e4561bb7482c3e472722bf57d3ba0c383f6261ff
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-30 20:16:54 +0300

    Complete task1

--- decision.py
+++ decision.py
@@ -1,1 +1,16 @@
-# задания распределены по веткам
+# Задайте список из нескольких чисел. Напишите программу,
+# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Пример:
+# o[2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+list = []
+count = 1
+sum = 0
+for i in range(1, random.randint(6, 20)):
+    list.append(random.randint(-10, 9))
+print(list, end=' -> ')
+while count < len(list):
+    sum += int(list[count])
+    count += 2
+print(sum)
