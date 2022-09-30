7f0cca44436832fe14db0bf7e7c9c1f212514266
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-09-30 16:35:06 +0300

    complete task3

--- decision.py
+++ decision.py
@@ -1,1 +1,11 @@
-## задания распределены по веткам
+# Задайте список из n чисел последовательности
+# 〖(1+1/n)〗^n и выведите на экран их сумму.
+import math
+import random
+n = random.randint(1, 5)
+print(n)
+list_seqence_to_n = []
+for i in range(1, n+1):
+    list_seqence_to_n.append((1+1/i)**i)
+print(list_seqence_to_n)
+print('Сумма элементов последовательности равна:', math.fsum(list_seqence_to_n))
