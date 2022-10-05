8d17d3a9d502a9e77b0c3a3eaf13ef4854d44f09
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-10-05 13:17:22 +0300

    complete task3 new decision

--- decision.py
+++ decision.py
@@ -6,15 +6,20 @@
 # o [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 import random
-list = []
-min = 1
-max = 0
+import math
+lst = []
+# min = 1
+# max = 0
 
 for i in range(0, random.randint(5, 10)):
-    list.append(round(random.uniform(1, 11), 2))
-for i in list:
-    if i % 1 > max:
-        max = i % 1
-    if i % 1 < min:
-        min = i % 1
-print(list, round((max-min), 2), sep=' => ')
+    lst.append(round(random.uniform(1, 11), 2))
+# for i in lst:
+#     if i % 1 > max:
+#         max = i % 1
+#     if i % 1 < min:
+#         min = i % 1
+# print(lst, round((max-min), 2), sep=' => ')
+print(lst, end=' => ')
+lst = [round(val % 1, 2) for val in lst]
+
+print(round((max(lst) - min(lst)), 2))
