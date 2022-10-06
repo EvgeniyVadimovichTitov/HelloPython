834f5190ffe3937305c42688219045cabd67a5bf
EvgeniyVadimovichTitov <jekon777999@gmail.com>
2022-10-06 21:38:07 +0300

    complete task4

--- decision.py
+++ decision.py
@@ -12,16 +12,22 @@ k = random.randint(1, 10)
 
 
 def create_equation(n, string=''):
-    for i in range(n, 0, -1):
+    for i in range(n, -1, -1):
         m = random.randint(0, 100)
         if i > 1 and m > 1:
-            string += str(m) + 'x^' + str(i) + '+'
+            string += '+' + str(m) + 'x^' + str(i)
         elif i > 1 and m == 1:
-            string += 'x^' + str(i) + '+'
+            string += '+' + 'x^' + str(i)
         elif i == 1 and m > 1:
-            string += str(m) + 'x' + '=0'
+            string += '+' + str(m) + 'x'
         elif i == 1 and m == 1:
-            string += 'x' + '=0'
+            string += '+' + 'x'
+        elif i == 0 and m > 0:
+            string += '+' + str(m) + '=0'
+        elif i == 0 and m == 0:
+            string += '=0'
+    if string[0] == '+':
+        string = string[1:]
     return string
 
 
